20445c6fa11b5b31e92747fa94565b208355cb70
Adrian Wenger <adwenger@htwg-konstanz.de>
2015-01-13 11:45:17 +0100

    Teil3.sql abgeschlossen und abgegeben

--- Praktikum/DBSYS_Aufgabe/SQL/Teil3.sql
+++ Praktikum/DBSYS_Aufgabe/SQL/Teil3.sql
@@ -14,28 +14,27 @@ Zahlungseingang DATE,
 KdNr NUMBER NOT NULL,
 Wohnung_Name varchar(60) NOT NULL,
 StornierungsDatum DATE NOT NULL,
-CONSTRAINT pk_STORNIERTE_BUCHUNGEN PRIMARY KEY(BuchungsNr));
+CONSTRAINT pk_STORNIERTE_BUCHUNGEN PRIMARY KEY(BuchungsNr)
+);
 
 Commit;
 
 CREATE OR REPLACE TRIGGER INSERT_STORNIERTE_BUCHUNGEN 
-BEFORE DELETE ON BUCHUNG
-FOR EACH ROW
-
-BEGIN
-
-INSERT INTO STORNIERTE_BUCHUNGEN (Datum, Bewertung, Zeitraum_von, Zeitraum_bis, Zahlungseingang, Wohnung_Name,KDNR, STORNIERUNGSDATUM) 
-VALUES (  :old.Datum,
-          :old.Bewertung,
-          :old.Zeitraum_von,
-          :old.Zeitraum_bis,
-          :old.Zahlungseingang,  
-          :old.Wohnung_Name,
-          :old.KDNR,
-          TO_CHAR(SYSDATE, 'DD.MM.YYYY'));
-      
+  BEFORE DELETE ON BUCHUNG
+  FOR EACH ROW
+  BEGIN
+    INSERT INTO STORNIERTE_BUCHUNGEN (Datum, Bewertung, Zeitraum_von, Zeitraum_bis, Zahlungseingang, Wohnung_Name,KDNR, STORNIERUNGSDATUM) 
+    VALUES (  :old.Datum,
+              :old.Bewertung,
+              :old.Zeitraum_von,
+              :old.Zeitraum_bis,
+              :old.Zahlungseingang,  
+              :old.Wohnung_Name,
+              :old.KDNR,
+              SYSDATE
+          );    
 END;
-
+/
 
 /* Insert Test-Datensatz */
 INSERT INTO BUCHUNG (Datum, Zeitraum_von, Zeitraum_bis, Wohnung_Name,KDNR) 
@@ -63,13 +62,18 @@ FROM BUCHUNG);
 Commit;
 
 /* View Kundenstatistik erstellen */
-CREATE VIEW Kundenstatistik AS 
-   SELECT Kunden.KDNR,  SumZahlungen.Summe, AnzBuchungen.AnzBuchungen, AnzStorno.AnzStorno
+CREATE OR REPLACE VIEW Kundenstatistik AS 
+   SELECT Kunden.KDNR,  SumZahlungen.Summe, COALESCE(AnzBuchungen.AnzBuchungen, 0) AS AnzBuchungen, AnzStorno.AnzStorno
    FROM Kunden LEFT OUTER JOIN (Select KDNR, Sum(Wert) as Summe From Buchung Group By KDNR Having Sum(Wert) > 0) SumZahlungen ON (SumZahlungen.KDNR = Kunden.KDNR)
    LEFT OUTER JOIN (SELECT KDNR, count(*) as AnzBuchungen FROM BUCHUNG GROUP BY KDNR) AnzBuchungen ON (KUNDEN.KDNR = AnzBuchungen.KDNR)
    LEFT OUTER JOIN (SELECT KDNR, count(*) as AnzStorno FROM STORNIERTE_BUCHUNGEN GROUP BY KDNR) AnzStorno ON (KUNDEN.KDNR = AnzStorno.KDNR)
 
 ;
+
+DELETE 
+FROM  BUCHUNG
+WHERE Buchungsnr = 4;
+commit;
 
 /* Muster Abfragen für From Klausel 
 /* Summe aller Zahlungen
